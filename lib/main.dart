eee8fd44517e3684c243fa738aba35555a65fde6
EfeDeveloper <edwinvilla_adsi@outlook.com>
2025-09-01 19:39:09 -0500

    fix(main): ensure async initialization for Firebase and dotenv
    - Fixes main.dart to properly initialize Firebase and dotenv using async main method
    - Ensures multiplatform secret management and app startup reliability
    
    [main][firebase][dotenv][init]

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,13 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:my_finance/app_theme.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:my_finance/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await dotenv.load();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MainApp());
 }
 
